d7c93522adc22fd366c1bfbb641f270d22da22e2
PBoyanov <pepiboyanovbg@gmail.bg>
2017-03-06 12:48:04 +0200

    add eslint check

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,5 +1,17 @@
 const gulp = require("gulp");
+const eslint = require("gulp-eslint");
 
-gulp.task("test", () => {
-    console.log("This is test!");
+
+gulp.task("default", () => {
+    console.log("I am default!");
+});
+
+gulp.task("lint:js", () => {
+    return gulp.src(["**/*.js", "!node_modules/**"])
+        .pipe(eslint({
+            configFile: "./eslint.json"
+        })
+        )
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
