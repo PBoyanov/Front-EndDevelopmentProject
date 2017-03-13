a3d25f87e5fdd62103ef704c7a674f71cfc163f8
PBoyanov <pepiboyanovbg@gmail.bg>
2017-03-13 22:58:42 +0200

    navbar - current item logic

--- public/js/events.js
+++ public/js/events.js
@@ -1,3 +1,11 @@
+$("#navbarTogglerDemo01").on('click', '.nav-item', function (event) {
+    let currentItem = $("#navbarTogglerDemo01").find(".current-menu-item");  
+    currentItem.removeClass("current-menu-item");
+
+    let targetListItem = $(event.target).parent();
+    targetListItem.addClass("current-menu-item");
+});
+
 $(window).on('load', function () {
-            $('#content').removeClass('loading');
+    $('#content').removeClass('loading');
 });
