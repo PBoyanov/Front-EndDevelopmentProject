b73c1a244d344c48484351e33e81fe3b755f3b61
PBoyanov <pepiboyanovbg@gmail.bg>
2017-03-20 13:27:36 +0200

    sites page - main content

--- public/js/sites.js
+++ public/js/sites.js
@@ -2,16 +2,32 @@ import { templateLoader } from './template-loader';
 import { data } from './data';
 
 let sites = (() => {
+    let templateItems = {};
+
     function getSitesPage(context) {
         let serverResponseSites;
 
         Promise.all([data.getSites(), templateLoader.get("all-sites")])
             .then(([serverResponseSites, template]) => {
                 let sites = serverResponseSites.data;
+                //console.log(sites);
                 processSitesData(sites);
+                templateItems.sites = sites;
 
-                let homeHtml = template(sites);
-                context.$element().html(homeHtml);
+                let pageHtml = template(templateItems);
+                console.log(pageHtml);
+                context.$element().html(pageHtml);
+
+                $(".site").hover(
+                    function mouseIn() {
+                        let itemWrap = $(this).find(".site-wrap");
+                        itemWrap.addClass("hover");
+                    },
+                    function mouseOut() {
+                        let itemWrap = $(this).find(".site-wrap");
+                        itemWrap.removeClass("hover");
+                    }
+                );
             });
     }
 
