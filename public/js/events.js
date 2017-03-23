606349e26d4eaa26bff2e6d9bcfb3383bd2ff239
PBoyanov <pepiboyanovbg@gmail.bg>
2017-03-23 11:04:03 +0200

    events.js - replace let with var

--- public/js/events.js
+++ public/js/events.js
@@ -2,13 +2,13 @@ $("#navbarTogglerDemo01").on("click", ".nav-item", function (event) {
     removeCurrentMenuItem("#navbarTogglerDemo01");
     removeCurrentMenuItem("#footer-menu-right");
 
-    let targetListItem = $(event.target).parent();
+    var targetListItem = $(event.target).parent();
     targetListItem.addClass("current-menu-item");
 
     //footer navbar
-    let footerMenuClass = checkElementForClass(targetListItem, "-link");
+    var footerMenuClass = checkElementForClass(targetListItem, "-link");
     if (footerMenuClass) {
-        let footerMenuItem = $("#footer-menu-right").find("." + footerMenuClass);
+        var footerMenuItem = $("#footer-menu-right").find("." + footerMenuClass);
         footerMenuItem.addClass("current-menu-item");
     }
 });
@@ -20,26 +20,26 @@ $("#navbarTogglerDemo01").on("click", "a", function () {
 $("#footer-menu-right").on("click", ".nav-item", function (event) {
     removeCurrentMenuItem("#footer-menu-right");
 
-    let targetListItem = $(event.target).parent();
+    var targetListItem = $(event.target).parent();
     targetListItem.addClass("current-menu-item");
 
     //header navbar
-    let footerMenuClass = checkElementForClass(targetListItem, "-link");
+    var footerMenuClass = checkElementForClass(targetListItem, "-link");
     if (footerMenuClass) {
         removeCurrentMenuItem("#navbarTogglerDemo01");
-        let headerMenuItem = $("#navbarTogglerDemo01").find("." + footerMenuClass);
+        var headerMenuItem = $("#navbarTogglerDemo01").find("." + footerMenuClass);
         headerMenuItem.addClass("current-menu-item");
     }
 });
 
 function removeCurrentMenuItem(selector) {
-    let currentFooterItem = $(selector).find(".current-menu-item");
+    var currentFooterItem = $(selector).find(".current-menu-item");
     currentFooterItem.removeClass("current-menu-item");
 }
 
 function checkElementForClass(element, className) {
-    let elementClasses = element.prop('className').split(' ');
-    let targetClass = elementClasses.find(function(el) {
+    var elementClasses = element.prop('className').split(' ');
+    var targetClass = elementClasses.find(function(el) {
         return el.indexOf(className) > 0;
     });
 
