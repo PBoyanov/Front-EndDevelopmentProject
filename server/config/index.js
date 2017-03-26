71e53d2f6a40221cdd96729a0ee9cda1e3a0d896
PBoyanov <pepiboyanovbg@gmail.bg>
2017-03-26 17:42:07 +0300

    heroku credentials

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,7 @@
 let port = process.env.PORT || 3000;
 let connection = process.env.MONGODB_URI || "mongodb://localhost/hundredtouristsites";
 let url = process.env.NODE_ENV || "http://localhost:3000";
-let herokuConnectionString = "mongodb://heroku_sff40t7w:ueu3fo3si6q62tb4q0ko45gvjr@ds141328.mlab.com:41328/heroku_sff40t7w";
+let herokuConnectionString = "mongodb://heroku_c3l8sv5j:849vvjs6in34pqgc6e52721dgu@ds137550.mlab.com:37550/heroku_c3l8sv5j";
 
 module.exports = {
     rootUrl: url,
